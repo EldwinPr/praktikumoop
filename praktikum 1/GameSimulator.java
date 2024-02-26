9d09f363ebdb2c8bd7a848364439b674df321d04
Eldwin Pradipta <EldwinPradipta670@gmail.com>
2024-02-26 10:38:25 +0700

    prak 1

---
+++ praktikum 1/GameSimulator.java
@@ -0,0 +1,24 @@
+import java.lang.System;
+import java.util.Scanner;
+
+public class GameSimulator {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int player = Integer.parseInt(scanner.nextLine());
+        String seeker = scanner.nextLine();
+        HideNSeek game = new HideNSeek(seeker, player);
+        System.out.println("Game dimulai dengan " + player + " pemain, seeker adalah " + seeker);
+        boolean ongoing = true;
+        while (ongoing){
+            int input = Integer.parseInt(scanner.nextLine());
+            if (input == game.getPlayerFound()+1){
+                System.out.println(input + " Pemain ditemukan");
+                game.foundPlayer();
+            }
+            if (game.getPlayerFound() == game.getPlayer()-1){
+                ongoing = false;
+                System.out.println("Semua pemain telah ditemukan, permainan selesai.");
+            }
+        }
+    }
+}
